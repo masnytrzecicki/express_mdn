aa41683bb8070a4cf2fffccf65c3ebfa2f428fdc
masnytrzecicki <kubatrzeciak00@gmail.com>
2021-09-13 13:45:53 +0200

    accesing the balls.........

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 8000;
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
 
+module.exports = {
+    area: function(width) {
+        return width * width;
+    },
+
+    perimeter: function(width) {
+        return 4 * width;
+    }
+};
 // Create HTTP server
 const server = http.createServer((req, res) => {
 
@@ -19,4 +30,6 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 })
+
+
 
--- express.js
+++ express.js
@@ -2,9 +2,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the balls.....');
+    next(); // pass control to the next handler
+});
+
+
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+module.exports = {
+    area: function(width) {
+        return width * width;
+    },
+
+    perimeter: function(width) {
+        return 4 * width;
+    }
+};
+console.log('First');
+console.log('Second');
+console.log('nigga balls');
+
+setTimeout(function() {
+    console.log('balls in ya');
+}, 3);
+setTimeout(function () {
+    console.log('jaws');
+}, 3);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
